b1105adb6934b78de31f72c4948a49f5b22da997
Joey Liu <joeyliu224@gmail.com>
2020-01-08 22:35:10 -0500

    server handle post request

--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const moment = require('moment');
 mongoose.connect('mongodb://localhost/reviews_lite',{ useNewUrlParser: true })
         .then(() => console.log('connected'))
 //schema        
@@ -6,13 +7,9 @@ const reviewsSchema = mongoose.Schema({
   _id: String,
   reviews: Array
 })
-// const characteristicsSchema = mongoose.Schema({
-//   _id: String,
-//   characteristics: Array
-// })
+
 //model
 const Review = mongoose.model('Review', reviewsSchema, 'reviews')
-//const Characteristics = mongoose.model('Characteristics',characteristicsSchema,'characteristics')
 
 //query db by id
 const retreive = (id,count,page) => {
@@ -38,8 +35,31 @@ const retreive = (id,count,page) => {
           // .exec();
 }
 
-const save = (review) => {
-
+const save = (prod_id,data) => {
+  data["reviewer_name"] = data["name"];
+  delete data["name"];
+  data["reviewer_email"] = data["email"];
+  delete data["email"];
+  data["reported"] = "0";
+  data["helpfulness"] = "0";
+  data["product_id"] = prod_id;
+  data["date"] = moment().format().slice(0,10);
+  if(data["recommend"] === true) {
+    data["recommend"] = "1";
+  } else {
+    data["recommend"] = "0";
+  }
+  console.log(data)
+  return Review
+          .findByIdAndUpdate(
+            prod_id,
+            {$push: {reviews: data}}
+          )
+          .catch(
+            (err) => {
+              console.log(err)
+            }
+          )
 }
 
 module.exports.retreive = retreive;
--- server/index.js
+++ server/index.js
@@ -15,8 +15,14 @@ app.get(`/reviews/:product_id/list`,(req,res) => {
   const page = req.query.page||0;
   db.retreive(id,count,page).then(
     (data) => {
-      //console.log(data)
-      res.status(200).send(data);
+      //format data
+      const response ={
+        product: id,
+        page: page,
+        count: count,
+        results: data
+      }
+      res.status(200).send(response);
     }
   ).catch(
     (err) => {
@@ -28,6 +34,33 @@ app.get(`/reviews/:product_id/list`,(req,res) => {
   )
 })
 
+// add a review:
+app.post('/reviews/:product_id',(req,res) => {
+  let data = req.body;
+  const id = req.params.product_id;
+  data["reviewer_name"] = data["name"];
+  delete data["name"];
+  data["reviewer_email"] = data["email"];
+  delete data["email"];
+  data["reported"] = "0";
+  data["helpfulness"] = "0";
+  data["product_id"] = id;
+  data["date"] = Date.now();
+  if(data["recommend"] === true) {
+    data["recommend"] = "1";
+  } else {
+    data["recommend"] = "0";
+  }
+  console.log(data)
+  db.save(id,data).then(
+    res.sendStatus(201)
+  ).catch(
+    (err) => {
+      console.log("error:",err)
+    }
+  );
+  
+})
 
 
 app.listen(port, () => console.log('Server running...'));
